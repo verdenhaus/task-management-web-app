0b6f6a53c77e76a04a2e7f75863a37a4b042992b
Dana <dana.0730@mail.ru>
2025-10-12 22:59:56 +0500

    Lab 1 Setup: Initialized Spring Boot project

---
+++ src/test/java/com/example/taskmanagementwebapp/TaskManagementWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
