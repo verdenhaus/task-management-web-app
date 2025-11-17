1fd015809e497aee9d54fd399472bb4011abd31c
Dana <dana.0730@mail.ru>
2025-11-17 22:38:53 +0500

    Lab 11: Adding CRUD functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import Carlist from './components/Carlist';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -15,6 +20,9 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
     </Container>
   );
 }
